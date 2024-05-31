4848932f88a1fb4d9c97c77679f44603c60ccfe8
Tiago Chacon <tiagochacon80@gmail.com>
2024-05-31 16:17:09 -0400

    starting the tests

--- Euro2024AppConsole/Program.cs
+++ Euro2024AppConsole/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Euro2024AppConsole.Models;
+
+namespace Euro2024AppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TeamService teamService = new TeamService();
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("Eurocup cup table 2024");
+                Console.WriteLine();
+            }
+        }
+    }
+}
