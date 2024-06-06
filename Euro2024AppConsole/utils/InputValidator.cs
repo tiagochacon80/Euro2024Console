82e35a256fd105c52af983a33e412dc6f34844b2
Tiago Chacon <tiagochacon80@gmail.com>
2024-06-06 10:53:23 -0400

    class input validator and method validator

---
+++ Euro2024AppConsole/utils/InputValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euro2024AppConsole.utils
+{
+    public static class InputValidator
+    {
+        public static int GetValidatedInput(string prompt)
+        {
+            int result;
+            while (true)
+            {
+                Console.Write(prompt);
+                if (int.TryParse(Console.ReadLine(), out result))
+                {
+                    break;
+                } 
+                else
+                {
+                    Console.WriteLine("Invalid input, please enter a valid number");
+                }
+            }
+            return result;
+        }
+    }
+}
