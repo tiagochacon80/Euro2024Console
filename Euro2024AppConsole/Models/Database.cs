695f9fadcb7744bce49ce64747cb472cfedbd99a
Tiago Chacon <tiagochacon80@gmail.com>
2024-06-09 18:21:38 -0400

    Configuring and Database class

--- Euro2024AppConsole/Models/Database.cs
+++ Euro2024AppConsole/Models/Database.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Euro2024AppConsole.Models
 {
-    internal class Database
+    public class Database
     {
+        public string connectionString;
+        
+        public Database(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
     }
 }
